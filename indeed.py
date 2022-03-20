ef1030eda3d40f3c6b43eddfe48c993a6d7dd1a5
SGianna <99999344+SGianna@users.noreply.github.com>
2022-03-20 04:25:48 +0000

    complete to scrapping jobs for python in 'indeed.com'

--- indeed.py
+++ indeed.py
@@ -19,18 +19,40 @@ def extract_indeed_pages():
     
     max_page = pages[-1]
     return max_page
+    
+def extract_job(html) :
 
+    title = html.find("span", title=True).string
+    
+    company = html.find("span",{"class":"companyName"})
+    company_anchor = company.find("a")
+    
+    if company_anchor is not None:
+        company = company_anchor.string
+    else:
+        company = company.string
+    
+    company = company.strip()
+
+    location = html.find("div",{"class":"companyLocation"})
+    if location.string is None:
+        location = location.text
+    else:
+        location = location.string  
+
+    job_id = html["data-jk"]
+    
+    return {'title' : title, 'company' : company, 'location' : location, 'link' : f"https://ca.indeed.com/viewjob?jk={job_id}&q=Python"}
+
+    
 def extract_indeed_jobs(last_page):
     jobs = []
-    #for page in range(last_page):
-    result = requests.get(f"{URL}&start={0*LIMIT}")
-    soup = BeautifulSoup(result.text, "html.parser")
-    results = soup.find_all("a",{"class":"fs-unmask"})
-    
-    for result in results:
-        jobTitle = result.find("h2", {"class": "jobTitle"})
-        title = jobTitle.find("span").string
-        if title == "new":
-            title = jobTitle.find_all("span")[1].string
-        print(title)
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        result = requests.get(f"{URL}&start={last_page*LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("a",{"class":"fs-unmask"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)       
     return jobs
--- main.py
+++ main.py
@@ -5,4 +5,6 @@ os.system('clear')
 
 last_indeed_pages = extract_indeed_pages()
 
-extract_indeed_jobs(last_indeed_pages)
+indeed_jobs = extract_indeed_jobs(last_indeed_pages)
+
+print(indeed_jobs)
